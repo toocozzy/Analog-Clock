cbae244e13efc01a0a0fd582ba6ce27f18d009b8
Oskar Daszkowski <oskar.daszkowski@gmail.com>
2021-10-21 18:40:43 +0200

    add text representation of date

--- js/main.js
+++ js/main.js
@@ -16,3 +16,54 @@ const clock = () => {
 };
 
 setInterval(clock, 1000);
+
+const textHour = document.getElementById("text-hour"),
+  textMinutes = document.getElementById("text-minutes"),
+  textAmPm = document.getElementById("text-ampm"),
+  dateDay = document.getElementById("date-day"),
+  dateMonth = document.getElementById("date-month"),
+  dateYear = document.getElementById("date-year");
+
+const clockText = () => {
+  let date = new Date();
+
+  let hh = date.getHours(),
+    mm = date.getMinutes(),
+    day = date.getDate(),
+    month = date.getMonth(),
+    year = date.getFullYear();
+
+  if (hh < 10) {
+    `0${hh}`;
+  }
+
+  if (mm < 10) {
+    mm = `0${mm}`;
+  }
+
+  textHour.innerHTML = `${hh}:`;
+  textMinutes.innerHTML = mm;
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  dateDay.innerHTML = day;
+  dateMonth.innerHTML = `${months[month]},`;
+  dateYear.innerHTML = year;
+};
+
+setInterval(clockText, 1000);
+
+// show time
